610e42e3bbd50fcccf57234cd6ef625c9bac865b
Volodymyr Potiichuk <exucounter@gmail.com>
2020-08-16 15:50:51 +0300

    Media queries

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,17 +1,22 @@
 let burgerMenu = document.querySelector('.burger-menu');
 let menu = document.querySelector('.header-nav');
-let headerMobileLogo = document.querySelector('.header-mobile-nav__logo');
+let headerMobileNav = document.querySelector('.header-mobile-nav');
 
 burgerMenu.addEventListener('click', () => {
+    if (document.documentElement.scrollTop > 0) {
+        $('html, body').animate({
+            scrollTop: $('body').offset().top
+        }, 500);
+    }
     if (!(menu.classList.contains('active'))) {
         menu.classList.add('active');
         burgerMenu.classList.add('active');
         document.body.style.overflow = 'hidden';
-        headerMobileLogo.classList.add('active');
+        headerMobileNav.classList.add('active');
     } else {
         menu.classList.remove('active');
         burgerMenu.classList.remove('active');
         document.body.style.overflow = 'scroll';
-        headerMobileLogo.classList.remove('active');
+        headerMobileNav.classList.remove('active');
     }
 })
