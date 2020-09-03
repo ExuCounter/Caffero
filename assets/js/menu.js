7bea47e1609f284cc0d44540d092c2673f9bcb8c
Volodymyr Potiichuk <exucounter@gmail.com>
2020-09-03 18:20:59 +0300

    Upd

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -9,14 +9,24 @@ burgerMenu.addEventListener('click', () => {
         }, 500);
     }
     if (!(menu.classList.contains('active'))) {
+        document.querySelector('.header-mobile-nav .header-mobile-nav__logo').style.opacity = '0';
         menu.classList.add('active');
         burgerMenu.classList.add('active');
-        document.body.style.overflowY = 'hidden';
+        document.querySelector('body').style.overflowY = 'hidden';
         headerMobileNav.classList.add('active');
     } else {
+        document.querySelector('.header-mobile-nav .header-mobile-nav__logo').style.opacity = '1';
         menu.classList.remove('active');
         burgerMenu.classList.remove('active');
-        document.body.style.overflowY = 'scroll';
+        document.querySelector('body').style.overflowY = 'scroll';
         headerMobileNav.classList.remove('active');
     }
 })
+
+if (document.documentElement.offsetWidth < 768) {
+    document.querySelectorAll('.mobile-prevent-default').forEach(link => {
+        link.onclick = (e) => {
+            e.preventDefault();
+        }
+    })
+}
